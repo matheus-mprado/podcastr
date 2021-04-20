c1b3d68137cd08198b726502d2f95ada8eebdd94
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2021-04-20 12:37:30 -0300

    player page

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,6 @@
+export function Player(){
+
+    return(
+        <div></div>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Player />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
