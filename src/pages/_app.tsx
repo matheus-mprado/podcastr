ed0c05cf8ca765d193c13b2c54cc6994468d106b
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2021-04-21 14:22:41 -0300

    alteração styles and player compoents

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
+import '../styles/global.scss'
+
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
-import '../styles/global.scss'
+
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
+    <div className={styles.wrapper}>
+
+
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
       <Player />
-      <Component {...pageProps} />
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ episodes }) {
+
   return (
-    <h1>Hello World</h1>
+
+    <>
+      <h1>Hello World</h1>
+
+    </>
   )
+}
+
+export async function getStaticProps() {
+  const responser = await fetch("http://localhost:3333/episodes")
+  const data = await responser.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
