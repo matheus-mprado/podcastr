829b46078d54bfee2485aeb840b2d146497c8b68
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2021-04-21 15:02:14 -0300

    ssg and map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,89 @@
-import Head from 'next/head'
+import { format, parseISO } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { GetStaticProps } from 'next'
 
-export default function Home({ episodes }) {
+import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/converteDurationToTimeString'
+
+import styles from '../styles/pages/home.module.scss'
+
+interface Episodes {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+}
+
+interface HomeProps {
+  latestEpisodes:Episodes[];
+  allEpisodes:Episodes[];
+}
+
+export default function Home({ latestEpisodes,allEpisodes }: HomeProps) {
 
   return (
+    <div className={styles.homePage} >
+      <section className={styles.latestEpisodes} >
+        <h2>Últimos Lançamentos</h2>
 
-    <>
-      <h1>Hello World</h1>
+        <ul>
+          {latestEpisodes.map(episode=>{
 
-    </>
+            return(
+              <li key={episode.id}>
+                <a href="">{episode.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        
+      </section>
+    </div>
   )
 }
 
-export async function getStaticProps() {
-  const responser = await fetch("http://localhost:3333/episodes")
-  const data = await responser.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
+  
+  const latestEpisodes = episodes.slice(0,2);
+
+  const allEpisodes = episodes.slice(2,episodes.length)
+
 
   return {
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes
     },
     revalidate: 60 * 60 * 8
   }
